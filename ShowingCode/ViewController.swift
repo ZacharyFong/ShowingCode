f6da0c7957a385f62b0bc6cbe0a462e33df3b5f7
ZacharyFong <weng200a@gmail.com>
2018-11-18 19:02:31 -0800

    Showing Code

--- ShowingCode/ViewController.swift
+++ ShowingCode/ViewController.swift
@@ -8,11 +8,72 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController {
+    
+    let products = ["iPhone", "iPad", "iPod", "Mac"]
 
+    fileprivate func setupNavigaionView() {
+        navigationItem.title = "Products"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Refresh", style: .plain, target: self, action: #selector(handleRefresh))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Sign out", style: .plain, target: self, action: #selector(handleSignout))
+    }
+    
+    let cellId = "cellid"
+    let headerId = "headerid"
+    
+    fileprivate func setUpCollectionView() {
+       collectionView?.backgroundColor = .white
+       collectionView?.register(ProductCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView?.register(Productheader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: headerId)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupNavigaionView()
+        setUpCollectionView()
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return products.count ?? 0
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        return cell
+    }
+    
+    
+    
+    @objc func handleRefresh() {
+      collectionView?.reloadData()
+        print("successful refresh")
+    }
+    
+    @objc func handleSignout() {
+        
+    }
+    
+    class Productheader: UICollectionViewCell {
+        
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            backgroundColor = .blue
+        }
+        required init?(coder aDecoder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+    }
+    
+    class ProductCell: UICollectionViewCell {
+        
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            backgroundColor = .red
+            }
+        
+        required init?(coder aDecoder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
     }
 
 
