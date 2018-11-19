91b5944e2233323b28f5f754d82d67fd7ac5dd17
ZacharyFong <weng200a@gmail.com>
2018-11-18 18:23:40 -0800

    Initial Commit

---
+++ ShowingCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShowingCode
+//
+//  Created by Zachary on 18/11/2018.
+//  Copyright © 2018 Chin Weng Fong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
